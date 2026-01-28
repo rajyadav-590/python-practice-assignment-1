ac788755cb10c7e3821470d2cbf8ac4d369a0f0b
rajyadav-590 <rajyadavetah590@gmail.com>
2026-01-28 19:07:06 +0530

    add task_1.py and task_2.py files

---
+++ task_1.py
@@ -0,0 +1,19 @@
+#taking two inputs as number from user
+num_1=float(input("Enter the first number: "))
+num_2=float(input("Enter the second number: "))
+#performing basic mathematical operations on two numbers
+
+#performing addition
+add=num_1+num_2
+
+#performing subtraction
+sub=num_1-num_2
+
+#performing multiplication
+mul=num_1*num_2
+
+#performing division
+div=num_1/num_2
+
+#printing results
+print("Addition: "+str(add),"Substraction: "+str(sub),"Multiplication: "+str(mul),"Division: "+str(div),sep="\n")
---
+++ task_2.py
@@ -0,0 +1,10 @@
+#taking user input as first name and last name 
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+
+#concatenating the first name and last name and storing in a full_name variable
+full_name=first_name+" "+last_name
+
+#printing or displaying personalized greeting message 
+# print("Hello,",full_name,"! Welcome to the Python program.")
+print("Hello, "+full_name+"! Welcome to the Python program.")
